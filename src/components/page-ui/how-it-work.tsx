14efc29809ba97d63ffa1562caa4818929c18759
redaezziani <2003100500167@ofppt-edu.ma>
2024-06-29 14:04:30 +0000

    chore: Update Arabic translations and improve UI for "How It Works" page

--- src/components/page-ui/how-it-work.tsx
+++ src/components/page-ui/how-it-work.tsx
@@ -1,16 +1,13 @@
 export const HowITWork = () => {
   return (
     <div className='mt-24 flex max-w-6xl flex-col items-center justify-center gap-2'>
-        <h1
-        className="text-4xl font-bold text-primary"
-        >
-          كيف تعمل الخدمة
-        </h1>
-        <p
-        className="text-center text-slate-400"
-        >
-          نقدم لكم خدمة شحن الرصيد لتطبيقات الألعاب والتداول بأمان تام وبأسعار منافسة.
-        </p>
+      <h3 className="text-4xl font-bold text-primary">
+        كيف تعمل الخدمة
+      </h3>
+      <p className="text-center text-slate-400">
+        نحن نقدم خدمة شحن الرصيد لتطبيقات الرهان والتداول بأمان تام وبأسعار تنافسية.
+      </p>
+
     </div>
   )
 }
