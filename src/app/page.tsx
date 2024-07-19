17613b880efe686a25d7306b20da766afa67c629
redaezziani <klausdev2@gmail.com>
2024-07-19 12:45:26 +0100

    chore: Update page UI with Arabic translations, fix button styling, and improve font loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,11 @@ import { ServiceSection } from '@/components/page-ui/service-section';
 import { MainSection } from '@/components/page-ui/main-section';
 import { InfiniteSlider } from '@/components/page-ui/slider';
 import { HowITWork } from '@/components/page-ui/how-it-work';
-import ClientRev from '@/components/page-ui/client-rev';
 import AccountCreationInfo from '@/components/page-ui/account-creationinfo';
 
 export default function Home() {
   return (
     <main className='relative flex min-h-screen flex-col items-center justify-start gap-2'>
-      {/* <div className="w-full left-0 top-0 z-0 h-full min-h-screen bg-gradient-to-b from-primary/20 via-amber-100/20 to-transparent absolute "/> */}
       <Header />
       <Layer />
       <MainSection />
